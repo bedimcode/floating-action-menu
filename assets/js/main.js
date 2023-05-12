a2de6a1af8f967476ad7bc5eec2097760d91a60d
Marlon Pilco (Bedimcode) <bedimcode@gmail.com>
2023-05-12 18:06:38 -0500

    Source code is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 /*=============== SHOW & HIDE MENU ===============*/
+const toggleButton = document.getElementById('floating-toggle')
+
+const activeMenu = () =>{
+    toggleButton.classList.toggle('active')
+}
+
+toggleButton.addEventListener('click', activeMenu)
